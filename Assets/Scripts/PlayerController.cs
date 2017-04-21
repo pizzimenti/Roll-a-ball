8edb2b2a137039a75be59c699c304cc1cd916b20
Brad Pizzimenti <brad@gennakersystems.com>
2017-04-21 11:32:13 -0700

    initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	private Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		spawnPoint = transform.position;
+	}
+
+
+	void FixedUpdate () { 	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+
+		if (transform.position.y < -10) {
+			transform.position = spawnPoint;
+		}
+	}
+}
